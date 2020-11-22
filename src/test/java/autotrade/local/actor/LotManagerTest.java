22e5dc1b9d157eae0b044234122ce4175de9eced
yuji_oota <yuji.oota@gmail.com>
2020-11-22 17:30:29 +0900

    テストコミットを修正

--- src/test/java/autotrade/local/actor/LotManagerTest.java
+++ src/test/java/autotrade/local/actor/LotManagerTest.java
@@ -10,28 +10,28 @@ public class LotManagerTest {
     public void test() {
         LotManager lotManager = new LotManager();
         lotManager.changePair(CurrencyPair.EURUSD, 100000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 200000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 300000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 400000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 500000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 600000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 700000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 800000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 900000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 1000000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 1500000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
         lotManager.changePair(CurrencyPair.EURUSD, 2000000);
-        System.out.println(lotManager.getInitial());
+        System.out.println(lotManager.getInitial() + ", " + lotManager.getLimit());
     }
 }
