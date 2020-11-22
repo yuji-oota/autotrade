52c536cf201b1c07edbb893ada42320d2bd8d6ac
yuji_oota <yuji.oota@gmail.com>
2020-11-23 07:01:17 +0900

    Sixthに変更

--- src/main/java/autotrade/local/Application.java
+++ src/main/java/autotrade/local/Application.java
@@ -1,7 +1,7 @@
 package autotrade.local;
 
 import autotrade.local.actor.AutoTrader;
-import autotrade.local.actor.AutoTraderFifth;
+import autotrade.local.actor.AutoTraderSixth;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -9,7 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        AutoTrader autoTrader = new AutoTraderFifth();
+        AutoTrader autoTrader = new AutoTraderSixth();
         try {
             while(true) {
                 autoTrader.operation();
