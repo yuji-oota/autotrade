377f66290e3428c3fa87d869423959ba36b85fd9
unknown <yuji.oota@gmail.com>
2021-06-14 01:21:33 +0900

    15th追加

--- src/main/java/autotrade/local/Application.java
+++ src/main/java/autotrade/local/Application.java
@@ -1,7 +1,7 @@
 package autotrade.local;
 
 import autotrade.local.actor.AutoTrader;
-import autotrade.local.actor.AutoTrader14th;
+import autotrade.local.actor.AutoTrader15th;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -9,7 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        AutoTrader autoTrader = new AutoTrader14th();
+        AutoTrader autoTrader = new AutoTrader15th();
         try {
             while(true) {
                 autoTrader.operation();
---
+++ src/main/java/autotrade/local/actor/AutoTrader15th.java
@@ -0,0 +1,214 @@
+package autotrade.local.actor;
+
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import org.openqa.selenium.By;
+
+import autotrade.local.material.Rate;
+import autotrade.local.material.Snapshot;
+import autotrade.local.utility.AutoTradeProperties;
+import autotrade.local.utility.AutoTradeUtils;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AutoTrader15th extends AutoTrader {
+
+    private enum OrderDirection {ASK, BID, NONE}
+
+    private RecoveryManager recoveryManager;
+    private OrderDirection orderDirection;
+    private Rate lastDayBeforeRate;
+
+    public AutoTrader15th() {
+        super();
+        recoveryManager = new RecoveryManager();
+        pairAnalyzerMap.values().stream().forEach(analyzer -> {
+            analyzer.setThresholdDuration(
+                    Duration.ofSeconds(
+                            AutoTradeProperties.getInt("autoTrader15th.rateAnalizer.threshold.seconds")));
+        });
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("do you need local load? (y or any) :");
+            String input = scanner.next();
+            if ("y".equals(input.toLowerCase())) {
+                Snapshot snapshot = AutoTradeUtils.localLoad(Paths.get("localSave", "snapshotWhenRecoveryStart"));
+                recoveryManager.open(snapshot);
+            }
+        };
+
+        //シャットダウンフックでローカルセーブ
+        Runtime.getRuntime().addShutdownHook(new Thread(
+            () -> {
+                AutoTradeUtils.localSave(Paths.get("localSave", "snapshotWhenRecoveryStart"), recoveryManager.getSnapshotWhenStart());
+            }
+        ));
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        String theDayBefore = driver.findElement(By.xpath("//*[@id=\"hl-div\"]/span[5]")).getText();
+        log.info("theDayBefore:{}", theDayBefore);
+        // lastDayBeforeRate を作成する
+        Snapshot snapshot = buildSnapshot();
+        lastDayBeforeRate = Rate.builder().ask(0).bid(0).timestamp(LocalDateTime.now()).build();
+        int lastDayBeforeBid = 0;
+        if (Pattern.matches("\\d", theDayBefore.substring(0, 1))) {
+            lastDayBeforeBid = AutoTradeUtils.toInt(theDayBefore);
+        }
+        lastDayBeforeRate.setBid(snapshot.getRate().getBid() - lastDayBeforeBid);
+        lastDayBeforeRate.setAsk(lastDayBeforeRate.getBid() + pair.getMinSpread());
+    }
+
+    @Override
+    protected boolean isOrderable(Snapshot snapshot) {
+        boolean isOrderable = super.isOrderable(snapshot);
+        return isOrderable;
+    }
+
+    @Override
+    protected void order(Snapshot snapshot) {
+
+        Rate rate = snapshot.getRate();
+        int initialLot = snapshot.getMargin() / 100000;
+
+        switch (snapshot.getStatus()) {
+        case NONE:
+            // ポジションがない場合
+
+            if (isPlusTheDayBefore()) {
+                if (rateAnalyzer.isAskUp()
+                        && rateAnalyzer.isReachedAskThreshold(rate)) {
+                    orderAsk(initialLot);
+                    recoveryManager.close();
+                    recoveryManager.open(snapshot);
+                    orderDirection = OrderDirection.ASK;
+                }
+            } else {
+                if (rateAnalyzer.isBidDown()
+                        && rateAnalyzer.isReachedBidThreshold(rate)) {
+                    orderBid(initialLot);
+                    recoveryManager.close();
+                    recoveryManager.open(snapshot);
+                    orderDirection = OrderDirection.BID;
+                }
+            }
+
+
+            break;
+        case ASK_SIDE:
+            // 買いポジションが多い場合
+        case BID_SIDE:
+            // 売りポジションが多い場合
+        case SAME:
+            // ポジションが同数の場合
+
+
+            if (isPlusTheDayBefore()) {
+                if (rateAnalyzer.isAskUp()) {
+                    if (orderDirection == OrderDirection.BID
+                            && snapshot.getAskLot() < lotManager.getLimit()
+                            && rateAnalyzer.isReachedAskThreshold(rate)) {
+                        if (snapshot.getAskLot() == snapshot.getBidLot()) {
+                            orderAsk(initialLot / 2);
+                        } else {
+                            orderAsk(1);
+                        }
+                        orderDirection = OrderDirection.ASK;
+                    }
+                    if (snapshot.getAskLot() < snapshot.getBidLot()
+                            && rateAnalyzer.isReachedAskThreshold(rate)) {
+                        forceSame(snapshot);
+                    }
+                    if (rateAnalyzer.isReachedAskThreshold(rate)) {
+                        orderDirection = OrderDirection.ASK;
+                    }
+                }
+            } else {
+                if (rateAnalyzer.isBidDown()) {
+                    if (orderDirection == OrderDirection.ASK
+                            && snapshot.getBidLot() < lotManager.getLimit()
+                            && rateAnalyzer.isReachedBidThreshold(rate)) {
+                        if (snapshot.getAskLot() == snapshot.getBidLot()) {
+                            orderBid(initialLot / 2);
+                        } else {
+                            orderBid(1);
+                        }
+                        orderDirection = OrderDirection.BID;
+                    }
+                    if (snapshot.getAskLot() > snapshot.getBidLot()
+                            && rateAnalyzer.isReachedBidThreshold(rate)) {
+                        forceSame(snapshot);
+                    }
+                    if (rateAnalyzer.isReachedBidThreshold(rate)) {
+                        orderDirection = OrderDirection.BID;
+                    }
+                }
+            }
+
+        default:
+        }
+
+    }
+
+    private boolean isPlusTheDayBefore() {
+        return true;
+    }
+
+    @Override
+    protected boolean isCalm() {
+        return rateAnalyzer.rangeWithin(Duration.ofSeconds(150)) < 25;
+    }
+
+    @Override
+    protected boolean isFixable(Snapshot snapshot) {
+        boolean isFixable = super.isFixable(snapshot);
+        if (isFixable
+                && snapshot.isPositionSame()
+                && lotManager.isLimit(snapshot)) {
+            isFixable = false;
+        }
+        return isFixable;
+    }
+
+    @Override
+    protected void fix(Snapshot snapshot) {
+
+        Rate rate = snapshot.getRate();
+
+        if (recoveryManager.isOpen()
+                && recoveryManager.isRecoveredWithProfit(snapshot, snapshot.getMargin() / 10000)) {
+            if (rateAnalyzer.isBidDown()
+                    && snapshot.isPositionAskSide()
+                    && rateAnalyzer.isReachedBidThreshold(rate)) {
+                fixAll(snapshot);
+            }
+            if (rateAnalyzer.isAskUp()
+                    && snapshot.isPositionBidSide()
+                    && rateAnalyzer.isReachedAskThreshold(rate)) {
+                fixAll(snapshot);
+            }
+            return;
+        }
+
+    }
+
+    @Override
+    protected void fixAll(Snapshot snapshot) {
+        super.fixAll(snapshot);
+        recoveryManager.printSummary(snapshot);
+        recoveryManager.close();
+    }
+
+    @Override
+    protected MessageListener customizeMessageListener() {
+        MessageListener messageListener = super.customizeMessageListener();
+        return messageListener;
+    }
+
+}
