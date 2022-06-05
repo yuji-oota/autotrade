ae66c3c5e96cf1545fdff5632caea34c7680e68a
yuji.oota <yuji.oota@gmail.com>
2022-06-05 10:20:23 +0900

    マルチプロジェクト化

---
+++ autotrade-local/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+cd `dirname $0`
+java -Dfile.encoding=UTF-8 -jar ./autotrade-local.jar
+
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-./gradlew
-java -Dfile.encoding=UTF-8 -jar autotrade-local.jar
-
